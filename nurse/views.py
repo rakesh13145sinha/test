4f441549593c7a30ccbf2046bdac0691278af385
rakesh13145sinha <rakeshsinha8292@gmail.com>
2022-04-04 20:07:54 +0530

    first commit

---
+++ nurse/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+# Create your views here.
+class Test(APIView):
+    def get(self,request):
+        return Response({"message":"test is successfull"})
